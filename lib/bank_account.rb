aa3ab507ddb31b63d65e05e1ef5839b9c7f5c075
Michael Boroski <mjboroski@gmail.com>
2018-04-26 03:56:52 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
 
+  attr_reader :name
+
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name=name
+    @balance=1000
+    @status="open"
+  end
+
+  def deposit
+  end
+
 end
